54587d32e4a676613f509fa987b88d825093594f
Diego Urrutia Astorga <durrutia@ucn.cl>
2017-04-05 20:54:37 -0300

    Added: Implementacion basica del chat.

--- src/main/java/ProcessRequestRunnable.java
+++ src/main/java/ProcessRequestRunnable.java
@@ -1,8 +1,11 @@
 import com.google.common.base.Stopwatch;
+import com.google.common.collect.Lists;
+import com.google.common.io.Resources;
 
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.io.LineIterator;
 import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.commons.lang3.RandomUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,7 +13,9 @@ import org.slf4j.LoggerFactory;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.net.URL;
 import java.nio.charset.Charset;
+import java.util.List;
 
 /**
  * @author Diego Urrutia Astorga <durrutia@ucn.cl>
@@ -22,6 +27,11 @@ public class ProcessRequestRunnable implements Runnable {
      * Logger de la clase
      */
     private static final Logger log = LoggerFactory.getLogger(ProcessRequestRunnable.class);
+
+    /**
+     * Contenedor de Chat
+     */
+    private static final List<String> chats = Lists.newArrayList();
 
     /**
      * Socket asociado al cliente.
@@ -58,17 +68,17 @@ public class ProcessRequestRunnable implements Runnable {
 
         log.debug("Connection from {} in port {}.", socket.getInetAddress(), socket.getPort());
 
-        // A dormir por un segundo.
+        // A dormir!
         try {
-            Thread.sleep(100);
+            Thread.sleep(RandomUtils.nextInt(100, 500));
         } catch (InterruptedException e) {
-            e.printStackTrace();
+            log.error("Error in sleeping", e);
         }
 
         try {
             processRequest(this.socket);
         } catch (Exception ex) {
-            log.error("Error", ex);
+            log.error("Error processing request", ex);
         } finally {
             try {
                 socket.close();
@@ -77,7 +87,7 @@ public class ProcessRequestRunnable implements Runnable {
             }
         }
 
-        log.debug("Request procesed in {}.", stopWatch);
+        log.debug("Request timex: {}.", stopWatch);
 
     }
 
@@ -103,11 +113,97 @@ public class ProcessRequestRunnable implements Runnable {
         // HTTP header
         writeHeader(outputStream);
 
-        // HTTP Body
-        writeBody(outputStream, request);
+        // Dividir el request
+        final String[] request3 = StringUtils.split(request);
+
+        // String from http protocol: "GET /chat HTTP/1.1"
+
+        // Cada componente
+        final String verbo = request3[0];
+        final String uri = request3[1];
+        final String version = request3[2];
+
+        // Deteccion de version
+        if (!StringUtils.equals("HTTP/1.1", version)) {
+            log.warn("Wrong version: {}", version);
+        }
+
+        // Si la peticion incluye /chat
+        if (StringUtils.startsWith(uri, "/chat")) {
+            writeChat(outputStream, verbo, uri);
+        } else {
+            // HTTP Body
+            writeBody(outputStream, request);
+        }
 
         // Cierro el stream
         IOUtils.closeQuietly(outputStream);
+
+    }
+
+    /**
+     * @param outputStream
+     * @param verbo
+     * @param uri
+     */
+    private static void writeChat(final OutputStream outputStream, final String verbo, final String uri) throws IOException {
+
+        if (StringUtils.contains(uri, "chat?msgText=")) {
+            final String msg = StringUtils.substringAfter(uri, "chat?msgText=");
+            log.debug("Msg to include: {}", msg);
+
+            // Sincronizacion
+            synchronized (chats) {
+                chats.add(msg);
+            }
+        }
+
+        // Listado completo de chat
+        final StringBuffer sb = new StringBuffer();
+
+        // Linea de chat
+        final String chatline = readFile("chatline.html");
+
+        // Siempre el mismo comportamiento
+        synchronized (chats) {
+            for (String line : chats) {
+                sb.append(StringUtils.replace(chatline, "CONTENT", line));
+                sb.append("\r\n");
+            }
+        }
+
+        // Contenido completo
+        final String content = readFile("index.html");
+
+        final String contentChat = StringUtils.replace(content, "<!-- CHAT_CONTENT-->", sb.toString());
+
+        // Envio el contenido
+        IOUtils.write(contentChat + "\r\n", outputStream, Charset.defaultCharset());
+
+    }
+
+    /**
+     * @param filename
+     * @return the contenido del archivo.
+     */
+    private static String readFile(final String filename) {
+
+        // URL del index
+        URL url;
+        try {
+            url = Resources.getResource(filename);
+        } catch (IllegalArgumentException ex) {
+            log.error("Can't find file", ex);
+            return null;
+        }
+
+        // Contenido
+        try {
+            return IOUtils.toString(url, Charset.defaultCharset());
+        } catch (IOException ex) {
+            log.error("Error in read", ex);
+            return null;
+        }
 
     }
 
@@ -167,6 +263,9 @@ public class ProcessRequestRunnable implements Runnable {
      * @param request
      */
     private static void writeBody(OutputStream outputStream, String request) throws IOException {
+
+        // Detect if chat is what we want
+
 
         // Body
         final String body = "<html><head><title>WebServer v1.0</title></head><body><h3>Result:</h3><pre>CONTENT</pre></body></html>";
